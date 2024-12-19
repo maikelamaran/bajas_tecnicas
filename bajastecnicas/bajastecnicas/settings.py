9a7cddd56b07ac780ff0ed24d1df537541f281bc
maikel <mikerosy10@gmail.com>
2024-12-18 18:55:41 -0800

    cambiando la bd a mysql

--- bajastecnicas/bajastecnicas/settings.py
+++ bajastecnicas/bajastecnicas/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'bajastecnicas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bajas_tecnicas',  # Cambia esto al nombre de tu base de datos en MySQL
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',  # O 'localhost', dependiendo de tu configuración
+        'PORT': '3307',  # Puerto de MySQL
     }
 }
 
