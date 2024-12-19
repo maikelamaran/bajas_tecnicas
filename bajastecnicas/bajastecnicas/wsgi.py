12461bd8194c62c47f45385f70c292e5fcc2a665
maikel <mikerosy10@gmail.com>
2024-12-19 14:55:09 -0800

    actualizando archivo wsgi.py

--- bajastecnicas/bajastecnicas/wsgi.py
+++ bajastecnicas/bajastecnicas/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for bajastecnicas project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Asegúrate de que la ruta al directorio del proyecto esté en sys.path
+sys.path.append('/home/mikerosy/bajas_tecnicas')  # Cambia esta ruta si es necesario
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bajastecnicas.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
